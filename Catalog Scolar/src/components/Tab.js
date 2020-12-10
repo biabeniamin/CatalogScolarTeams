81cfbe34d5e5c2c00eb18104775eedae701ee2d5
Beniamin Bia <biabeniamin@outlook.com>
2020-12-10 19:06:30 +0200

    make http request to get classes

--- Catalog Scolar/src/components/Tab.js
+++ Catalog Scolar/src/components/Tab.js
@@ -4,6 +4,7 @@
 import React from 'react';
 import './App.css';
 import * as microsoftTeams from "@microsoft/teams-js";
+import axios from 'axios';
 
 /**
  * The 'GroupTab' component renders the main tab content
@@ -14,7 +15,7 @@ class Tab extends React.Component {
     super(props)
     this.state = {
       context: {},
-      classes: {}
+      classes: [{ name: ''}]
     }
   }
 
@@ -27,6 +28,15 @@ class Tab extends React.Component {
         context: context
       });
     });
+
+    axios.get('http://localhost/catalog/ClassRooms.php?cmd=getClassRooms')
+            .then(res => {
+                this.setState({ classes: res.data });
+                console.log(res.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     // Next steps: Error handling using the error object
   }
 
@@ -38,6 +48,7 @@ class Tab extends React.Component {
       <div>
         <h3>Hello World!</h3>
         <h1>Congratulations {userName}!</h1> <h3>This is the tab you made :-)</h3>
+        {this.state.classes[0].name}
       </div>
       );
   }
