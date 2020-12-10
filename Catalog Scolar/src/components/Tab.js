547ad8813cbc4b9371aad543c20bb2cc2aa41cbd
Beniamin Bia <biabeniamin@outlook.com>
2020-12-10 19:15:29 +0200

    display data as table

--- Catalog Scolar/src/components/Tab.js
+++ Catalog Scolar/src/components/Tab.js
@@ -1,27 +1,46 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import * as microsoftTeams from "@microsoft/teams-js";
 import axios from 'axios';
+
+class DataTable extends Component {
+  render() {
+      return (
+          <tr>
+              <td>
+                  {this.props.obj.classRoomId}
+              </td>
+              <td>
+                  {this.props.obj.name}
+              </td>
+              <td>
+                  {this.props.obj.creationTime}
+              </td>
+          </tr>
+      );
+  }
+}
+
 
 /**
  * The 'GroupTab' component renders the main tab content
  * of your app.
  */
 class Tab extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       context: {},
-      classes: [{ name: ''}]
+      classes: [{ name: '' }]
     }
   }
 
   //React lifecycle method that gets called once a component has finished mounting
   //Learn more: https://reactjs.org/docs/react-component.html#componentdidmount
-  componentDidMount(){
+  componentDidMount() {
     // Get the user context from Teams and set it in the state
     microsoftTeams.getContext((context, error) => {
       this.setState({
@@ -30,27 +49,45 @@ class Tab extends React.Component {
     });
 
     axios.get('http://localhost/catalog/ClassRooms.php?cmd=getClassRooms')
-            .then(res => {
-                this.setState({ classes: res.data });
-                console.log(res.data);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+      .then(res => {
+        this.setState({ classes: res.data });
+        console.log(res.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     // Next steps: Error handling using the error object
+  }
+
+  dataTable() {
+    return this.state.classes.map((data, i) => {
+        return <DataTable obj={data} key={i} />;
+    });
   }
 
   render() {
 
-      let userName = Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "";
+    let userName = Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "";
 
-      return (
+    return (
       <div>
         <h3>Hello World!</h3>
         <h1>Congratulations {userName}!</h1> <h3>This is the tab you made :-)</h3>
         {this.state.classes[0].name}
+        <table className="table table-striped table-dark">
+          <thead className="thead-dark">
+            <tr>
+              <td>ID</td>
+              <td>Name</td>
+              <td>CreationTime</td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.dataTable()}
+          </tbody>
+        </table>
       </div>
-      );
+    );
   }
 }
 export default Tab;
